d5dadfc3310ec249aa688dabb83274b4e4a78e86
ceronquillo <157269675+ceronquillo@users.noreply.github.com>
2024-01-29 20:21:51 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+
+namespace Homework2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the grade(A,B,C,D, or F):");
+        char grade= char.ToUpper(Console.ReadKey().KeyChar);
+        int points;
+        switch (grade)
+        {case 'A':
+            points=4;
+            break;
+        case 'B':
+            points = 3;
+            break;
+        case 'C':
+            points=2;
+            break;
+        case 'D':
+            points=1;
+            break;
+        case 'F':
+            points=0;
+            break;
+        default:
+            Console.WriteLine("Wrong Letter Grade!");
+            return;}
+        double gpa=points;
+        Console.WriteLine($"ngrade {grade} corresponds to {points} points");
+        Console.WriteLine($"GPA point: {gpa}");
+    
+
+
+
+    }
+}
